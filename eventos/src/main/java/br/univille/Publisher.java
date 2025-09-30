098d5739389daf76d319a6088a3884784e3266f7
murilo <112010004+murilott@users.noreply.github.com>
2025-09-30 22:59:30 +0000

    feat: exemplo de fila

--- eventos/src/main/java/br/univille/Publisher.java
+++ eventos/src/main/java/br/univille/Publisher.java
@@ -1,8 +1,6 @@
 package br.univille;
 
 import com.azure.core.amqp.AmqpTransportType;
-import com.azure.identity.DefaultAzureCredential;
-import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.messaging.servicebus.ServiceBusClientBuilder;
 import com.azure.messaging.servicebus.ServiceBusMessage;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
